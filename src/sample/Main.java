7df54826d9bb17d530dbdd8f71e440b0ec4abb3c
gunter1994 <hunter.thompson@uoit.net>
2016-03-22 00:21:58 -0400

    fixed error with play again button

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -143,6 +143,7 @@ public class Main extends Application {
             @Override
             public void handle(ActionEvent event) {
                 Snake s = new Snake();
+                s.move(grid, dir);
                 stage.close();
                 play(grid, s, primaryStage);
             }
