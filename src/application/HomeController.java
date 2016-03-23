40890ef48b43f0c1a8033f39340f137a18ea2e25
SNHouse <samantha.house@uoit.net>
2016-03-23 16:48:29 -0400

    Started Customize Snake Menu Item

--- src/application/HomeController.java
+++ src/application/HomeController.java
@@ -3,14 +3,18 @@ package application;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * Created by samantha on 23/03/16.
  */
 public class HomeController {
 
-    @FXML
     public void handleNewGame(ActionEvent event) {
         MainGame g = new MainGame();
         try{
@@ -18,5 +22,20 @@ public class HomeController {
             g.start(primaryStage);
         }catch(Exception e){System.err.println();}
     }
+
+    public void customizeSnake(ActionEvent event) throws IOException {
+        Stage primaryStage = new Stage();
+        primaryStage.setTitle("Snake Customization");
+        Parent root = FXMLLoader.load(getClass().getResource("Customize_Snake.fxml"));
+        Scene customizeSnake = new Scene(root,300,300);
+        primaryStage.setScene(customizeSnake);
+        primaryStage.show();
+
+
+
+
+    }
+
+
 
 }
