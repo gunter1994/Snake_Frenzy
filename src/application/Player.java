d7d7a38ee2117b05b6bbb9a5c50b3211b49029b9
SNHouse <samantha.house@uoit.net>
2016-03-23 16:52:22 -0400

    created player class

---
+++ src/application/Player.java
@@ -0,0 +1,14 @@
+package application;
+
+/**
+ * Created by samantha on 23/03/16.
+ */
+public class Player {
+    String username;
+    Snake s;
+    int highscore;
+
+    public Player() {
+
+    }
+}
