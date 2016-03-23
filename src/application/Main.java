ea3eb06d498162a9d19505d219e6fd37e8cfebe5
SNHouse <samantha.house@uoit.net>
2016-03-23 11:10:13 -0400

    created login screen

---
+++ src/application/Main.java
@@ -0,0 +1,23 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        primaryStage.setTitle("Snake Frenzy Login");
+        primaryStage.setScene(new Scene(root, 350, 350)); //(w,h)
+
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
