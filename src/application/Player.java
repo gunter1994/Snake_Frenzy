b4c0f131fb97419f6258f50d784c4a436461fc1e
AdamOlivieri <adam.olivieri@uoit.net>
2016-04-06 13:20:38 -0500

    Added Highscore file maker but Commented out because it causes an error

--- src/application/Player.java
+++ src/application/Player.java
@@ -1,11 +1,40 @@
 package application;
 
+import java.io.*;
+
+
 public class Player {
 
     String username;
     int highscore;
+    File newFile = null;
 
     public Player() {
 
     }
+
+    /*public void HighScore(){
+        File newFile = new File("Highscore.csv");
+        if (newFile.exists())
+            System.out.println("");
+        else{
+            try{
+                newFile.createNewFile();
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String WriteHighScore(){
+        try{
+            FileWriter wFile = new FileWriter(newFile);
+            BufferedWriter buffWrite = new BufferedWriter(wFile);
+        }
+        catch(Exception e){
+            return "";
+        }
+
+    }*/
 }
